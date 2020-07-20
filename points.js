6d7d37aa4303216352e52aee8568548428012d78
Artiom <64665123+qbeeck@users.noreply.github.com>
2020-07-20 18:40:28 +0300

    Delete points.js

--- points.js
+++
@@ -1,8 +0,0 @@
-
-let value = ["c1", "c2", "c4"];
-let sum = 0;
-let pepe = [];
-value.forEach(el => pepe.push(+el.substr(1)));
-console.log(pepe);
-let q = pepe.forEach(element => sum += element);
-console.log(q);
