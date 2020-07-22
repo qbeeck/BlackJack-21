ecefbee622144d2f7e83d4a77af4ab2f46984a06
Artiom <64665123+qbeeck@users.noreply.github.com>
2020-07-22 21:46:23 +0300

    75%

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ***** elements *****
 const pScore = document.querySelector(".playerScore");
 const dScore = document.querySelector(".dealerScore");
 const gStatus = document.querySelector(".gameStatus");
@@ -6,103 +7,192 @@ const dHand = document.querySelector(".dealerHand");
 const hitBtn = document.querySelector(".hitButton");
 const standBtn = document.querySelector(".standButton");
 const nGameBtn = document.querySelector(".newGame");
-const wStat = document.querySelector(".winStat");
-const lStat = document.querySelector(".lossStat");
-const dStat = document.querySelector(".drawStat");
+const wStatus = document.querySelector(".winStat");
+const lStatus = document.querySelector(".lossStat");
+const dStatus = document.querySelector(".drawStat");
 // ***** events *****
-// hitBtn.addEventListener("click", hit);
-// standBtn.addEventListener("click", stand);
+hitBtn.addEventListener("click", hit);
+standBtn.addEventListener("click", stand);
 nGameBtn.addEventListener("click", newGame);
 // ***** values *****
 let deck = new Array();
-let pCards;
-let dCards;
-let pPoints = 0;
-let dPoints = 0;
-let pECards;
-let dECards;
 let startedGame = false;
+let gameStand = false;
+
+let playerCards = new Array();
+let playerCardsURL = new Array();
+let playerScore = 0;
+
+let dealerCards = new Array();
+let dealerCardsURL = new Array();
+let dealerScore = 0;
+
+let wins = 0;
+let loss = 0;
+let draw = 0;
 // ***** functions *****
-function newGame() {
-  if (!startedGame) {
-    gStatus.textContent = "Hit or Stand";
-    createDeck();
-    shuffleDeck();
-    playersCard();
-    checkPoints();
-    editArrCards();
-    pTable(pECards);
-    dTable(dECards);
-    startedGame = true;
-  } else if (startedGame) {
-    setBackToDefault();
-    startedGame = false;
-    pPoints = 0;
-    dPoints = 0;
-    newGame();
+let deckUtilities = {
+  createDeck() {
+    let suits = ["s", "d", "c", "h"];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+    for (let i = 0; i < suits.length; i++) {
+      for (let g = 0; g < values.length;g++) {
+        let card = suits[i] + values[g];
+        deck.push(card);
+      }
+    }
+  },
+  shuffleDeck() {
+    for (let i = 0; i < 1000; i++) {
+      let location1 = Math.floor((Math.random() * deck.length));
+      let location2 = Math.floor((Math.random() * deck.length));
+      let tmp = deck[location1];
+      deck[location1] = deck[location2];
+      deck[location2] = tmp;
+      
+    }
+  },
+};
+
+let cards = {
+  cardToPlayer(num) {
+    playerCards = deck.splice(0, num);
+  },
+  pCardsURL() {
+    playerCardsURL = playerCards.map(el => {return `${el}.png`});
+  },
+  cardToDealer(num) {
+    dealerCards = deck.splice(0, num);
+  },
+  dCardsURL() {
+    dealerCardsURL = dealerCards.map(el => {return `${el}.png`});
   }
-}
-function createDeck() {
-  let suits = ["s", "d", "c", "h"];
-  let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
-  for (let i = 0; i < suits.length; i++) {
-    for (let g = 0; g < values.length;g++) {
-      let card = suits[i] + values[g];
-      deck.push(card);
+};
+
+let table = {
+  pTable() {
+    playerCardsURL.map((el, node) => {
+      node = document.createElement("div");
+      node.classList.add("card","playerCard");
+      node.innerHTML = `
+                      <img src="./img/${el}">
+      `;
+      pHand.appendChild(node);
+    });
+  },
+  dTable() {
+    dealerCardsURL.map((el, node) => {
+      node = document.createElement("div");
+      node.classList.add("card","dealerCard");
+      node.innerHTML = `
+                      <img src="./img/${el}">
+      `;
+      dHand.appendChild(node);
+    });
+  },
+  dInverted() {
+    inverted = document.createElement("div");
+    inverted.classList.add("invCard", "card");
+    inverted.innerHTML = `
+                      <img src="./img/hidden.png">
+      `;
+      dHand.appendChild(inverted);
+  }
+};
+
+let scoreStatus = {
+  calculatePlayerScore() {
+    for (let value of playerCards) {
+      playerScore += +value.substr(1);
+    }
+  pScore.textContent = playerScore;
+  },
+  calculateDealerScore() {
+    for (let value of dealerCards) {
+      dealerScore += +value.substr(1);
+    }
+  dScore.textContent = dealerScore;
+  }
+};
+
+let deleteItems = {
+  deleteCards() {
+    let elements = document.getElementsByClassName("card");
+    while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+  },
+  deletePlayerCards() {
+    let elements = document.getElementsByClassName("playerCard");
+    while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+  },
+  deleteInvertedCard() {
+    let elements = document.getElementsByClassName("invCard");
+    while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+  },
+  deleteDealerCards() {
+    let elements = document.getElementsByClassName("dealerCard");
+    while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
     }
   }
 }
-function shuffleDeck() {
-  for (let i = 0; i < 1000; i++) {
-    let location1 = Math.floor((Math.random() * deck.length));
-    let location2 = Math.floor((Math.random() * deck.length));
-    let tmp = deck[location1];
-    deck[location1] = deck[location2];
-		deck[location2] = tmp;
-  }
-}
-function playersCard() {
-  pCards = deck.splice(0, 2);
-  dCards = deck.splice(0, 1);
-}
-function checkPoints() {
-  let lpPoints = new Array();
-  let ldPoints = new Array();
-  pCards.forEach(el => lpPoints.push(+el.substr(1)));
-  dCards.forEach(el => ldPoints.push(+el.substr(1)));
-  lpPoints.forEach(element => pPoints += element);
-  ldPoints.forEach(element => dPoints += element);
-  pScore.textContent = pPoints;
-  dScore.textContent = dPoints;
-}
-function editArrCards() {
-pECards = pCards.map(el => { return `${el}.png`});
-dECards = dCards.map(el => { return `${el}.png`});
-}
-function pTable(arr) {
-  arr.map((el, node) => {
-    node = document.createElement("div");
-    node.classList.add("card");
-    node.innerHTML = `
-                    <img src="./img/${el}">
-    `;
-    pHand.appendChild(node);
-  });
-}
-function dTable(arr) {
-  arr.map((el, node) => {
-    node = document.createElement("div");
-    node.classList.add("card");
-    node.innerHTML = `
-                    <img src="./img/${el}">
-    `;
-    dHand.appendChild(node);
-  });
-}
-function setBackToDefault() {
-  let elements = document.getElementsByClassName("card");
-  while(elements.length > 0){
-      elements[0].parentNode.removeChild(elements[0]);
+
+function newGame() {
+  if (!startedGame) {
+    deckUtilities.createDeck();
+    deckUtilities.shuffleDeck();
+    cards.cardToPlayer(2);
+    cards.cardToDealer(1);
+    cards.pCardsURL();
+    cards.dCardsURL();
+    table.pTable();
+    table.dInverted();
+    table.dTable();
+    scoreStatus.calculatePlayerScore();
+    scoreStatus.calculateDealerScore();
+    gStatus.textContent = "Hit or Stand";
+    startedGame = true;
+    hitBtn.disabled = false;
+    standBtn.disabled = false;
+    rule.checkScore();
+  } else if (startedGame) {
+    deleteItems.deleteCards();
+    startedGame = false;
+    playerScore = 0;
+    dealerScore = 0;
+    newGame();
   }
 }
 
+function hit() {
+  playerScore = 0;
+  playerCards = playerCards.concat(deck.splice(0, 1));
+  cards.pCardsURL();
+  deleteItems.deletePlayerCards();
+  table.pTable();
+  scoreStatus.calculatePlayerScore();
+  
+}
+
+function stand() {
+  dealerScore = 0;
+  dealerCards = dealerCards.concat(deck.splice(0, 1));
+  cards.dCardsURL();
+  deleteItems.deleteInvertedCard();
+  deleteItems.deleteDealerCards();
+  table.dTable();
+  scoreStatus.calculateDealerScore();
+  gameStand = true;
+}
+
+
+
+// Доделать stand, 
+//          набор карт пока у дилера не будет больше очков чем игрока
+//          правила игры
+//          может быть и локальное хранилище для статистики
